a7b2dafc652af02fda13cd310db6fcb795fe3247
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-03 10:55:58 +0000

    Generate and hide new secret key

--- sharesphere_drf_api/settings.py
+++ sharesphere_drf_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-atwktv48&+f&z$^#&7=+&e-wl%y^qq$!u*mux1^gb!k&0)-tko'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
