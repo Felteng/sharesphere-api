787ddad7666f1f59380ec1e2e906d826d55d39a0
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-03 16:54:58 +0000

    Add Post helper and Meta methods

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    """
+    Post model related to the User model.
+    
+    Posts are ordered by when they were created with the newest
+    ones visible at the top.
+
+    Returns a str representation with the owner and title of each post
+    to make it easier to identify a post.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,3 +20,9 @@ class Post(models.Model):
         upload_to='images/', default='DEFAULTS/post_default'
         )
     content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.owner.username}: {self.title}'
