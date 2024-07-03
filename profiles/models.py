89c9f4c793f249300e5e28dc490a2c7fd5921840
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-03 11:35:05 +0000

    Add and migrate profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', default='./DEFAULTS/profile_default')
+    
+  
