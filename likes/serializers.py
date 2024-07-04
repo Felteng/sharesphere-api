f18ba661aa45e419cc9ed97e03aec377d25cfb55
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-04 09:28:59 +0000

    Validate owner trying to like their own post

--- likes/serializers.py
+++ likes/serializers.py
@@ -14,6 +14,7 @@ class LikeSerializer(serializers.ModelSerializer):
     to create a like with the validated data and handles IntegrityError in
     case of a duplicate instance by raising a validation error with a
     descriptive message.
+    It also ensures that the owner of a post can't like their own post.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
@@ -22,8 +23,17 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner', 'post', 'created_at']
         read_only_fields = ['id', 'created_at']
 
+    # Duplicate validation from:
     # https://github.com/Code-Institute-Solutions/drf-api/blob/ed54af9450e64d71bc4ecf16af0c35d00829a106/likes/serializers.py#L17-L23
     def create(self, validated_data):
+        request = self.context.get('request')
+        post = validated_data.get('post')
+
+        if post.owner == request.user:
+            raise serializers.ValidationError({
+                'detail': 'You cannot like your own post.'
+            })
+
         try:
             return super().create(validated_data)
         except IntegrityError:
