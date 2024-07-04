e13ffc71cdfde628ba1e2f80d390ea36c07eb7ea
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-04 06:52:06 +0000

    Add DjangoFilterBackend to filter by post owner

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,20 @@ from .models import Post
 from rest_framework import generics, permissions, filters
 from .serializers import PostSerializer
 from sharesphere_drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListPosts(generics.ListCreateAPIView):
+    """
+    ListCreateAPIView to view all posts as well as create a post given
+    user is authenticated.
+
+    Override perform_create to ensure the owner field gets a value, the value
+    will be that of the user performing the request.
+
+    Posts can be filtered through searching the post owner's name or title as
+    well as filtered by posts made by a single owner.
+    """
     queryset = Post.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
@@ -13,9 +24,13 @@ class ListPosts(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
     filter_backends = [
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend
     ]
     search_fields = [
         'owner__username',
         'title',
+    ]
+    filterset_fields = [
+        'owner__profile'
     ]
