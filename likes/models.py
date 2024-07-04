5a4935944949c5f5e80143e12bc73122c50f2d47
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-04 08:23:45 +0000

    Ordering and unqiue_together for owner and post

--- likes/models.py
+++ likes/models.py
@@ -4,6 +4,18 @@ from posts.models import Post
 
 
 class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'post'.
+
+    Likes are ordered by when they were created with the newest ones
+    visible at the top.
+    To make sure that a post cannot be liked twice by one user unique_together
+    is utilised.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
