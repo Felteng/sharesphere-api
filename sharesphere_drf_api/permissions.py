9efb0bdd568a85ff59b3da6f34987cb478b477e2
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-05 08:40:01 +0000

    Add new permis. for message owners and receivers

--- sharesphere_drf_api/permissions.py
+++ sharesphere_drf_api/permissions.py
@@ -10,9 +10,25 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     'if request.method in permissions.SAFE_METHODS' ensures that any request
     has read permission.
 
-    Write permissions will only be allowed to the obj.owner of the profile.
+    Write permissions will only be allowed to the obj.owner.
     """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+
+class IsOwnerOrReceiver(permissions.BasePermission):
+    """
+    Custom permission intended for messages and replies to only allow owners
+    and receivers of a message to view it whilst only allowing the owners to
+    edit it.
+
+    'if obj.receiver == request.user and request.method in permissions.SAFE_METHODS'
+    ensures that only the receiver will always have read permission.
+    
+    Write and read permissions will only be allowed to the obj.owner.
+    """
+    def has_object_permission(self, request, view, obj):
+        if obj.receiver == request.user and request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
