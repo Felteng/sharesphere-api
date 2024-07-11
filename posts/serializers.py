0daf3f1969b061244450f5a389220258f4f1fb16
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-11 16:12:00 +0000

    Add owner image/id fields and like_id field

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,9 +6,12 @@ class PostSerializer(serializers.ModelSerializer):
     Serializer for the Post model.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
     is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """
@@ -37,8 +40,21 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
+    # https://github.com/Code-Institute-Solutions/drf-api/blob/ed54af9450e64d71bc4ecf16af0c35d00829a106/posts/serializers.py#L32-L39
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Likes.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'title', 'created_at', 'updated_at', 'image', 'content', 'likes_count', 'comments_count', 'is_owner']
+        fields = [
+            'id', 'owner', 'title', 'created_at', 'updated_at', 'image',
+            'content', 'likes_count', 'comments_count', 'is_owner',
+            'profile_image', 'profile_id', 'like_id'
+        ]
         read_only_fields = ['id', 'created_at', 'updated_at']
