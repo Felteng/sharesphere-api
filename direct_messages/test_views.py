cbd5d22716663314b773cb37e5962a85a496686a
Felix Tenggren <felixtenggren1@gmail.com>
2024-07-29 13:37:17 +0000

    Add tests for message in messages list

--- direct_messages/test_views.py
+++ direct_messages/test_views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import Message
 
 
-class TestDetailViews(TestCase):
+class TestMessagesViews(TestCase):
 
     def setUp(self):
         """User and Message creation setup"""
@@ -42,6 +42,81 @@ class TestDetailViews(TestCase):
             msg="myUsername2 is not the receiver"
         )
 
+    def test_retrieve_message_in_list_as_owner(self):
+        """Test that the owner can see the message in the /messages list"""
+        self.client.login(username="myUsername1", password="myPassword1")
+        response = self.client.get(
+            reverse('list_messages')
+        )
+        self.assertEqual(
+            response.status_code, 200,
+            msg='User could not retrieve message list'
+        )
+
+        for message in response.data['results']:
+            """Loop through list of messages to find created message"""
+            if message['id'] == self.message.pk:
+                self.assertEqual(
+                    message['topic'], self.message.topic,
+                    msg='Message topic in list does not match'
+                )
+                self.assertEqual(
+                    message["is_owner"], True,
+                    msg='User 1 is not owner of the message in the list'
+                )
+                self.assertEqual(
+                    message['is_receiver'], False,
+                    msg='User 1 is the receiver of the message in the list'
+                )
+                break
+
+    def test_retrieve_message_in_list_as_receiver(self):
+        """Test that the receiver can see the message in the /messages list"""
+        self.client.login(username="myUsername2", password="myPassword2")
+        response = self.client.get(
+            reverse('list_messages')
+        )
+        self.assertEqual(
+            response.status_code, 200,
+            msg='User could not retrieve message list'
+        )
+
+        for message in response.data['results']:
+            """Loop through list of messages to find created message"""
+            if message['id'] == self.message.pk:
+                self.assertEqual(
+                    message['topic'], self.message.topic,
+                    msg='Message topic in list does not match'
+                )
+                self.assertEqual(
+                    message["is_owner"], False,
+                    msg='User 2 is the owner of the message in the list'
+                )
+                self.assertEqual(
+                    message['is_receiver'], True,
+                    msg='User 2 is not the receiver of the message in the list'
+                )
+                break
+
+    def test_retrieve_message_in_list_as_other_user(self):
+        """Test that another user can't see message in the /messages list"""
+        self.client.login(username="myUsername3", password="myPassword3")
+        response = self.client.get(
+            reverse('list_messages')
+        )
+        self.assertEqual(
+            response.status_code, 200,
+            msg='User could not retrieve message list'
+        )
+
+        for message in response.data['results']:
+            """
+            Loop through list of messages to find created message.
+            If message is found forces test to fail.
+            """
+            if message['id'] == self.message.pk:
+                self.fail('Message is visible for user 3 in messages list')
+
     def test_retrieve_message_by_owner(self):
         """Test that the owner can retrieve the message"""
         self.client.login(username="myUsername1", password="myPassword1")
@@ -59,6 +134,10 @@ class TestDetailViews(TestCase):
         self.assertEqual(
             response.data['is_owner'], True,
             msg='User 1 is not the message owner'
+        )
+        self.assertEqual(
+            response.data['is_receiver'], False,
+            msg='User 1 is the message receiver'
         )
 
     def test_retrieve_message_by_receiver(self):
@@ -78,6 +157,10 @@ class TestDetailViews(TestCase):
         self.assertEqual(
             response.data['is_owner'], False,
             msg='User 2 is the message owner'
+        )
+        self.assertEqual(
+            response.data['is_receiver'], True,
+            msg='User 2 is not the message receiver'
         )
 
     def test_retrieve_message_by_other_user(self):
